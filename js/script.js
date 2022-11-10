7f65f1b69cfbc2433622e2ec72c9ed6eb82eaa84
unknown <mido.med75@gmail.com>
2022-11-10 16:01:08 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+var mybutton = document.getElementById("upBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// scroll to the top of the document
+function topFunction() {
+ 
+  document.documentElement.scrollTop = 0; 
+}
